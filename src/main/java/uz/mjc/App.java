8d15e27aeb97ac43c7ca874dd95f950dd030296d
doston turimov <dostonturimov19991107@gmail.com>
2024-10-10 17:31:30 +0500

    1 commit

---
+++ src/main/java/uz/mjc/App.java
@@ -0,0 +1,15 @@
+package uz.mjc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+
+}
